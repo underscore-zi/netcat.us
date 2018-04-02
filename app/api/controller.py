2a1c5ca336a00c7d301dfe263b06530a3d59c1fe
zizizi <gitlab@little-canada.org>
2018-04-02 01:53:57 -0600

    s/user/users in api routes

--- app/api/controller.py
+++ app/api/controller.py
@@ -8,19 +8,19 @@ import app.users.controller as users_module
 
 module = Blueprint('api', __name__, url_prefix='/api')
 
-@module.route("/user/name/<name>/")
+@module.route("/users/name/<name>/")
 def profile(name,api=False):
 	return users_module.profile(name,True)
 
-@module.route("/user/id/<id>/")
+@module.route("/users/id/<id>/")
 def profile_by_id(id,api=False):
 	return users_module.profile_by_id(id,True)
 
-@module.route("/user/leaderboard/")
+@module.route("/users/leaderboard/")
 def leaderboard():
 	return users_module.leaderboard(True)
 
-@module.route("/user/ranks/")
+@module.route("/users/ranks/")
 def ranks(api=False):
 	return users_module.ranks(True)
 
